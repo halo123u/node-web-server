153d5ee0de849a7fa4b17f93f7a59e4131d198ba
Oswaldo Almazo <oswaldo.almazo@gmail.com>
2017-03-21 17:20:53 -0400

    setting up basic templates for the website's

--- server.js
+++ server.js
@@ -1,22 +1,26 @@
 const express = require('express');
+const hbs = require('hbs');
 
 var app = express();
 
-app.get('/', (req,res)=>{
-    //res.send('<h1>hello express</h1>');
-    res.send({
-        name: 'oswaldo',
-        likes: [
-            'gaming',
-            'stuff',
+app.set('view engine', 'hbs');
+app.use(express.static(__dirname + '/public'));
 
-        ]
-    })
+app.get('/', (req,res)=>{
+    
+    res.render('home.hbs', {
+        pageTitle: 'Home Page',
+        currentYear: new Date().getFullYear(),
+        welcomeMessage: "Welcome to the Home Page!"
+
+    });
 });
 
 app.get('/about', (req, res)=>{
-    res.send('About Page');
-
+    res.render('about.hbs',{
+        pageTitle : 'About Page',
+        currentYear: new Date().getFullYear()
+    });
 });
 
 app.get('/bad', (req, res)=>{
@@ -25,4 +29,7 @@ app.get('/bad', (req, res)=>{
         code: '400'
     });
 });
-app.listen(3000);
+
+app.listen(3000, ()=>{
+    console.log('Server is up on port 3000')
+});
